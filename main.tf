877d7d54fb9a745d36b1ce092afe33fae9384235
mannem302 <102237426+mannem302@users.noreply.github.com>
2025-08-06 19:54:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,8 @@
-terraform {
-  required_providers {
-    fivetran = {
-      source  = "fivetran/fivetran"
-      version = ">= 1.0.0"
-    }
-  }
-}
-
 provider "fivetran" {
   api_key    = var.api_key
   api_secret = var.api_secret
 }
 
-variable "api_key" {}
-variable "api_secret" {}
 
 # Create a Fivetran group
 resource "fivetran_group" "my_group" {
