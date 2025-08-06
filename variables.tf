21e1c81e2c022d513e9493f3665129c80fea4e24
mannem302 <102237426+mannem302@users.noreply.github.com>
2025-08-06 19:56:06 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "api_key" {
+  description = "Fivetran API key"
+  type        = string
+  sensitive   = true
+}
+
+variable "api_secret" {
+  description = "Fivetran API secret"
+  type        = string
+  sensitive   = true
+}
