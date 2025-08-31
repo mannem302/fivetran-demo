e306002d5948ecf10e569e577a1f6c7891195042
mannem302 <102237426+mannem302@users.noreply.github.com>
2025-08-31 19:45:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,4 +34,33 @@ provider "fivetran" {
   api_secret = data.vault_kv_secret_v2.fivetran_creds.data["api_secret"]
 }
 
+# Fivetran group (Destination name)
+resource "fivetran_group" "my_group" {
+  name = var.fivetran_group_name
+}
+ 
+# Read snowflake private key from vault 
+data "vault_kv_secret_v2" "snowflake_private_key" {
+  mount = "secrets"
+  name  = "${var.fivetran_env_type}/fivetran/destinations/snowflake/dev/general/cred"
+}
+ 
+#Creation of fivetran destination (snowflake)
+resource "fivetran_destination" "snowflake_destination" {
+  group_id         = fivetran_group.my_group.id
+  service          = var.service_type
+  time_zone_offset = var.snowflake_timezone_offset
+  region           = var.snowflake_region 
+  config  {
+    connection_type = var.connection_type
+    host         = var.snowflake_host
+    port         = var.snowflake_port
+    database     = var.snowflake_database
+    user         = var.snowflake_user
+    role         = var.snowflake_role
+    auth         = var.auth_type
+	  private_key  = data.vault_kv_secret_v2.snowflake_private_key.data["private_key"]
+    passphrase   = data.vault_kv_secret_v2.snowflake_private_key.data["passphrase"]
+  }
+}
 
