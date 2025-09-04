f64a9d447b4700aca04dc8fd1f827d7a01d82c01
mannem302 <102237426+mannem302@users.noreply.github.com>
2025-09-04 17:26:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,35 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.18.0"
+    }
+    fivetran = {
+      source  = "fivetran/fivetran"
+      version = "1.9.5"
+    }
+  }
+}
 
+# ---------------------------
+# 1. Configure Vault Provider
+# ---------------------------
+provider "vault" {
+  # Auth-related info is passed from Terraform Cloud variables
+}
+
+# ---------------------------
+# 2. Fetch Fivetran API credentials from Vault
+# ---------------------------
+data "vault_kv_secret_v2" "fivetran_creds" {
+  mount = "secrets"
+  name  = "${var.fivetran_env_type}/fivetran/system_key/cred"
+}
+
+# ---------------------------
+# 3. Configure Fivetran Provider
+# ---------------------------
+provider "fivetran" {
+  api_key    = data.vault_kv_secret_v2.fivetran_creds.data["api_key"]
+  api_secret = data.vault_kv_secret_v2.fivetran_creds.data["api_secret"]
+}
