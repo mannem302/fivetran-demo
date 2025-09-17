33359bb387c8e066b6558a7f6cb1416c3ca919da
mannem302 <102237426+mannem302@users.noreply.github.com>
2025-09-17 07:54:43 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vault = {
       source  = "hashicorp/vault"
-      version = "3.18.0"
+      version = "5.2.1"
     }
     fivetran = {
       source  = "fivetran/fivetran"
